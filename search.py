b0efd30523f5009fef934ce7aa562461431e237c
murugancse <muruganaccetcse@gmail.com>
2021-08-05 07:33:46 +0530

    complete simple search program

--- search.py
+++ search.py
@@ -1,5 +1,30 @@
 import json
+from difflib import get_close_matches
 
 jsonfile = json.load(open("data1.json"))
 
-print(jsonfile)
+word = input("enter the word: ")
+
+def check(d):
+
+    d = d.lower().strip()
+
+    if d in jsonfile:
+        return jsonfile[d]
+    elif len(get_close_matches(d,jsonfile.keys())) > 0:
+        choice = input("Did you mean %s , Enter Y for YES, N for NO " %get_close_matches(d,jsonfile.keys())[0])
+        if choice == "Y" or "y":
+
+            return jsonfile[get_close_matches(d,jsonfile.keys())[0]]
+
+        elif choice == "N" or "n":
+
+            return "The word doesn't exist, please enter the correct word"
+
+        else:
+
+            return "you enterd the wrong choice"
+    else:
+        return "The word doesn't exist, please enter the correct word"
+
+print(check(word))
